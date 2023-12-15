4710ed5113cde3dd346753297836bbb31675cbba
GabrielNeves24 <2200495@my.ipleiria.pt>
2023-12-15 19:43:22 +0000

    +coiassa

--- index.js
+++ index.js
@@ -8,35 +8,29 @@ const io = require("socket.io")(httpServer, {
     }
 });
 
-httpServer.listen(8080, () =>{
-    console.log('listening on *:8080')
+httpServer.listen(8081, () =>{
+    console.log('listening on *:8081')
 })
 io.on('connection', (socket) => {
     console.log(`client ${socket.id} has connected`)
-    // socket.on('newTransaction', (transaction) => {
-    //     socket.broadcast.emit('newTransaction', transaction)
-    // })
-    // // socket.on('updateProject', (project) => {
-    // //     socket.broadcast.emit('updateProject', project)
-    // // })
-    // // socket.on('deleteProject', (project) => {
-    // //     socket.broadcast.emit('deleteProject', project)
-    // // })
-    // socket.on('loggedIn', function (user) {
-    //     // socket.join(user.id)
-    //     // if (user.type == 'A') {
-    //     //     socket.join('administrator')
-    //     // }
-    // })
-    // socket.on('loggedOut', function (user) {
-    //     socket.leave(user.id)
-    //     socket.leave('administrator')
-    // })
-    // socket.on('insertedUser', function (user) {
-    //     socket.in('administrator').emit('insertedUser', user)
-    // })
-    // socket.on('updatedUser', function (user) {
-    //     socket.in('administrator').except(user.id).emit('updatedUser', user)
-    //     socket.in(user.id).emit('updatedUser', user)
-    // })
+     socket.on('newTransaction', (transaction) => {
+         socket.broadcast.emit('newTransaction', transaction)
+     })
+     socket.on('loggedIn', function (user) {
+          socket.join(user.id)
+          console.log('testes')
+          if (user.user_type == 'A') {
+              socket.join('administrator')
+              console.log('login as administrator')
+              //socket.in('administrator').emit('loginUser', user)
+              io.to('administrator').emit('userLoggedIn', { userId: user.id });
+          }
+     })
+     socket.on('loggedOut', function (user) {
+         socket.leave(user.id)
+         socket.leave('administrator')
+     })
+     socket.on('insertedUser', function (user) {
+         socket.in('administrator').emit('insertedUser', user)
+     })
 })
