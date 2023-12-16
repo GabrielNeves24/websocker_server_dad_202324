3d058e7e379c1ff1c4a1e38a59a2247d5e9aa5dc
GabrielNeves24 <2200495@my.ipleiria.pt>
2023-12-16 18:37:27 +0000

    mais
    Conta bloqueada por admin num vcard,
    vcard recebe alerta e logout automático
    
    Transação credito de admin para vcard, se vcard login mostra a ele
    
    Transação debito de vcard para vcard, se vcard login mostra a ele
    
    Login admin, todos os admins ligados são avisados

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ httpServer.listen(8081, () =>{
 io.on('connection', (socket) => {
     console.log(`client ${socket.id} has connected`)
      socket.on('newTransaction', (transaction) => {
-         socket.broadcast.emit('newTransaction', transaction)
+        io.to(receiverVcardId).emit('newTransaction', transaction);
      })
      socket.on('loggedIn', function (user) {
           socket.join(user.id)
@@ -22,15 +22,36 @@ io.on('connection', (socket) => {
           if (user.user_type == 'A') {
               socket.join('administrator')
               console.log('login as administrator')
-              //socket.in('administrator').emit('loginUser', user)
-              io.to('administrator').emit('userLoggedIn', { userId: user.id });
+              socket.in('administrator').emit('loginUser', user)
+              //io.to('administrator').emit('userLoggedIn', user);
           }
+          if (user.user_type == 'V') {
+            socket.join(`vcard-${user.id}`); // Vcards join a specific room
+            console.log(`Vcard ${user.id} logged in`);
+        }
      })
-     socket.on('loggedOut', function (user) {
-         socket.leave(user.id)
-         socket.leave('administrator')
-     })
-     socket.on('insertedUser', function (user) {
-         socket.in('administrator').emit('insertedUser', user)
-     })
+
+     // Admin creates a transaction
+    socket.on('adminTransaction', (transaction) => {
+        const receiverVcardId = transaction.vcard; // ID of the vCard receiver
+        socket.in(`vcard-${receiverVcardId}`).emit('newTransaction', transaction);
+        console.log(`Transaction sent to vCard ${receiverVcardId}`);
+    });
+    socket.on('debitTransaction', (transaction) => {
+        const receiverVcard = transaction.reference;
+        io.to(`vcard-${receiverVcard}`).emit('receivedDebit', transaction);
+        console.log(`Debit transaction sent to vCard ${receiverVcard}`);
+    });
+    // User logs out
+    socket.on('loggedOut', (user) => {
+        socket.leave(user.id);
+        if (user.user_type === 'V') {
+            socket.leave(`vcard-${user.id}`);
+        }
+        console.log(`User ${user.id} logged out`);
+    });
+
+    socket.on('blockUser', (vcard) => {
+        socket.in(`vcard-${vcard}`).emit('blockUser', vcard);
+    });
 })
